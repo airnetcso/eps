134577f0c1ab65f7d0ad27133e30cd608c0b4abe
airnetcso <airnetcso@gmail.com>
2025-12-27 22:28:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ async function loadSoal(){
     const res = await fetch("https://raw.githubusercontent.com/airnetcso/eps/refs/heads/main/soal.json");
     questions = await res.json();
 
-    // hanya dashboard yg punya grid
+    // dashboard
     if(document.getElementById("listen")){
       buildGrid();
     }
 
-    // hanya halaman soal
+    // halaman soal
     if(document.getElementById("questionBox")){
       loadQuestionPage();
     }
@@ -33,34 +33,4 @@ function buildGrid(){
   L.innerHTML = "";
   R.innerHTML = "";
 
-  questions.forEach(q=>{
-    if(!q.id || !q.type) return;
-
-    const box = document.createElement("div");
-    box.className = "qbox";
-    box.textContent = q.id;
-
-    if(answered[q.id] !== undefined){
-      box.classList.add("done");
-    }
-
-    box.onclick = ()=>{
-      localStorage.setItem("current", q.id);
-      location.href = "question.html";
-    };
-
-    if(q.type.toLowerCase() === "listening"){
-      L.appendChild(box);
-    }else{
-      R.appendChild(box);
-    }
-  });
-}
-
-/* ================= HALAMAN SOAL ================= */
-function loadQuestionPage(){
-  const id = parseInt(localStorage.getItem("current"));
-  if(!id) return;
-
-  const idx = questions.findIndex(q=>q.id === id);
-  if(idx < 0) re
+  questions.forEa
